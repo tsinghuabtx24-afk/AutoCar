4370c8569524d6f85369f9138022d7e6930baad1
tsinghuabtx24-afk <tsinghuabtx24@gmail.com>
2026-09-01 17:22:23 +0800

    AutoCar

---
+++ Core/Inc/main.h
@@ -0,0 +1,81 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RRGB_R_Pin GPIO_PIN_2
+#define RRGB_R_GPIO_Port GPIOE
+#define RRGB_G_Pin GPIO_PIN_3
+#define RRGB_G_GPIO_Port GPIOE
+#define RRGB_B_Pin GPIO_PIN_4
+#define RRGB_B_GPIO_Port GPIOE
+#define LRGB_G_Pin GPIO_PIN_1
+#define LRGB_G_GPIO_Port GPIOG
+#define LRGB_R_Pin GPIO_PIN_7
+#define LRGB_R_GPIO_Port GPIOE
+#define LRGB_B_Pin GPIO_PIN_2
+#define LRGB_B_GPIO_Port GPIOG
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
